6425c0b339f99b6987163989db01436da1776a93
Panu Pitkamaki <pspi@users.noreply.github.com>
2021-10-01 18:44:57 +0300

    add import plugin

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
   rules: {
     "prefer-const": ["error"],
+    "import/first": "error",
+    "import/order": ["error", { "newlines-between": "always", groups: [["builtin", "external"]] }],
   }
 };
--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@
 npm install --save-exact --save-dev \
   bytearcher/eslint-config-cra \
   eslint-config-prettier \
+  eslint-plugin-import \
   eslint-plugin-prettier \
-  prettier
+  prettier \
 
 npx pkg-jq -i '.eslintConfig.extends += ["plugin:prettier/recommended", "@bytearcher/eslint-config-cra"]'
-#npx pkg-jq -i '.eslintConfig.rules + {"prettier/prettier": "error"}'
 
 npx eslint --fix src
 
