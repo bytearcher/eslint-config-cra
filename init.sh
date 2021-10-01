05cadea971884a4704e3a22a24c59c6a94bd0709
Panu Pitkamaki <pspi@users.noreply.github.com>
2021-10-01 18:37:49 +0300

    add eslint prettier plugin

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 # Install dependencies
 
-npm install --save-dev bytearcher/eslint-config-cra \
-                       eslint-config-prettier
+npm install --save-exact --save-dev \
+  bytearcher/eslint-config-cra \
+  eslint-config-prettier \
+  eslint-plugin-prettier \
+  prettier
 
-npx pkg-jq -i '.eslintConfig.extends += ["prettier", "@bytearcher/eslint-config-cra"]'
+npx pkg-jq -i '.eslintConfig.extends += ["plugin:prettier/recommended", "@bytearcher/eslint-config-cra"]'
+#npx pkg-jq -i '.eslintConfig.rules + {"prettier/prettier": "error"}'
 
 npx eslint --fix src
 
