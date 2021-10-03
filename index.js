c50db4698247c9290e359845a5b3f9e2c30a406a
Panu Pitkamaki <pspi@users.noreply.github.com>
2021-10-03 11:11:28 +0300

    adjust config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     "import/first": "error",
     "import/order": ["error", { "newlines-between": "always", groups: [["builtin", "external"]] }],
+    "object-shorthand": "error",
     "prefer-const": ["error"],
     "prettier/prettier": ["error", { "printWidth": 120 }]
   }
