b510e089e87c0b2aeeca1af97c6235dbecd2ba63
Panu Pitkamaki <pspi@users.noreply.github.com>
2022-09-15 20:04:32 +0300

    tunings

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 module.exports = {
   rules: {
     "import/first": "error",
-    "import/order": ["error", { "newlines-between": "always", groups: [["builtin", "external"]] }],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        groups: [
+          ["builtin", "external"],
+          ["internal", "object", "parent", "sibling", "index", "type"],
+          ["unknown"]],
+        pathGroups: [
+          {
+            pattern: "./*.+(scss|css)",
+            group: "unknown",
+            position: "after",
+          },
+        ],
+        warnOnUnassignedImports: true,
+      },
+    ],
     "object-shorthand": "error",
     "prefer-const": ["error"],
     eqeqeq: ["error"],
     "func-style": ["error", "declaration", { allowArrowFunctions: false }],
     "react/jsx-curly-brace-presence": ["error", "never"],
-  }
+    "padding-line-between-statements": ["error", { blankLine: "always", prev: "*", next: "return" }],
+    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+  },
 };
